4485f58f989f62e52066009d6bf71fcdd1871e5b
Christoph Kappestein <k42b3.x@gmail.com>
2012-11-11 21:19:18 +0100

    add html filter xss tests

---
+++ tests/PSX/Html/Filter/XssTestCase.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ *  $Id: FilterTest.php 560 2012-07-29 02:42:22Z k42b3.x@googlemail.com $
+ *
+ * psx
+ * A object oriented and modular based PHP framework for developing
+ * dynamic web applications. For the current version and informations
+ * visit <http://phpsx.org>
+ *
+ * Copyright (c) 2010-2012 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of psx. psx is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * psx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with psx. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Test html filter against several xss vectors to see how the filter behaves.
+ * The XSS vextors are taken from http://ha.ckers.org/xss.html
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://phpsx.org
+ * @category   tests
+ * @version    $Revision: 560 $
+ */
+class PSX_Html_Filter_XssTestCase extends PHPUnit_Framework_TestCase
+{
+	protected function setUp()
+	{
+	}
+
+	protected function tearDown()
+	{
+	}
+
+	public function assertXss($expected, $actual)
+	{
+		$filter = new PSX_Html_Filter($actual);
+
+		$this->assertEquals($expected, $filter->filter());
+	}
+
+	public function assertXssEmpty($actual)
+	{
+		$expected = <<<HTML
+
+HTML;
+
+		$this->assertXss($expected, $actual);
+	}
+}
