836e0a22c79035c67c6524aefa7ba491ff528940
Christoph Kappestein <k42b3.x@gmail.com>
2013-04-06 21:14:34 +0200

    add record store interface

---
+++ library/PSX/Data/RecordStoreInterface.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ *  $Id: StoreInterface.php 542 2012-07-10 20:20:59Z k42b3.x@googlemail.com $
+ *
+ * psx
+ * A object oriented and modular based PHP framework for developing
+ * dynamic web applications. For the current version and informations
+ * visit <http://phpsx.org>
+ *
+ * Copyright (c) 2010-2012 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of psx. psx is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * psx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with psx. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PSX\Data;
+
+/**
+ * StoreInterface
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://phpsx.org
+ * @category   PSX
+ * @package    PSX_Payment
+ * @version    $Revision: 542 $
+ */
+interface RecordStoreInterface
+{
+	public function save($key, RecordInterface $record);
+	public function load($key);
+}
