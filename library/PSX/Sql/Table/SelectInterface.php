c955901ba50601bca1cb6cdeae519b1f1542a88e
Christoph Kappestein <k42b3.x@gmail.com>
2013-04-30 09:49:23 +0200

    remove getResultSet from interface

--- library/PSX/Sql/Table/SelectInterface.php
+++ library/PSX/Sql/Table/SelectInterface.php
@@ -43,7 +43,6 @@ interface SelectInterface
 	public function orderBy($column, $sort = 0x1);
 	public function limit($start, $count = null);
 
-	public function getResultSet($startIndex = 0, $count = 32, $sortBy = null, $sortOrder = null, $filterBy = null, $filterOp = null, $filterValue = null, $updatedSince = null, $mode = 0, $class = null, array $args = array());
 	public function getAll($mode = 0, $class = null, array $args = array());
 	public function getRow($mode = 0, $class = null, array $args = array());
 	public function getCol();
