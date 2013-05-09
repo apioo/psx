b07206c1d83b638dd7f5a8dc57bad31655943f7e
Christoph Kappestein <k42b3.x@gmail.com>
2013-05-09 21:47:51 +0200

    add basic xrds writer

---
+++ tests/PSX/Xrds/WriterTest.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * psx
+ * A object oriented and modular based PHP framework for developing
+ * dynamic web applications. For the current version and informations
+ * visit <http://phpsx.org>
+ *
+ * Copyright (c) 2010-2013 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of psx. psx is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * psx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with psx. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PSX\Xrds;
+
+/**
+ * WriterTest
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link    http://phpsx.org
+ */
+class WriterTest extends \PHPUnit_Framework_TestCase
+{
+	public function testWriter()
+	{
+		$writer = new Writer();
+		$writer->addService('http://www.myopenid.com/server', array('http://specs.openid.net/auth/2.0/signon'));
+		$writer->addService('http://www.myopenid.com/server', array('http://specs.openid.net/auth/2.0/signon'), 20);
+
+		$actual   = $writer->toString();
+		$expected = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<xrds:XRDS xmlns="xri://$xrd*($v*2.0)" xmlns:xrds="xri://$xrds">
+  <XRD>
+    <Service>
+      <Type>http://specs.openid.net/auth/2.0/signon</Type>
+      <URI>http://www.myopenid.com/server</URI>
+    </Service>
+    <Service priority="20">
+      <Type>http://specs.openid.net/auth/2.0/signon</Type>
+      <URI>http://www.myopenid.com/server</URI>
+    </Service>
+  </XRD>
+</xrds:XRDS>
+XML;
+
+		$this->assertXmlStringEqualsXmlString($expected, $actual);
+	}
+}
