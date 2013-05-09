80daababc701f5bc450d2189c6d4e564508e6635
Christoph Kappestein <k42b3.x@gmail.com>
2013-05-09 23:30:27 +0200

    update doc

--- bin/check-imports.php
+++ bin/check-imports.php
@@ -1,25 +1,24 @@
 <?php
 /*
- *  $Id: update-service.php 836 2012-08-26 21:54:07Z k42b3.x@googlemail.com $
+ * psx
+ * A object oriented and modular based PHP framework for developing
+ * dynamic web applications. For the current version and informations
+ * visit <http://phpsx.org>
  *
- * amun
- * A social content managment system based on the psx framework. For
- * the current version and informations visit <http://amun.phpsx.org>
+ * Copyright (c) 2010-2013 Christoph Kappestein <k42b3.x@gmail.com>
  *
- * Copyright (c) 2010-2012 Christoph Kappestein <k42b3.x@gmail.com>
- *
- * This file is part of amun. amun is free software: you can
+ * This file is part of psx. psx is free software: you can
  * redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or any later version.
  *
- * amun is distributed in the hope that it will be useful,
+ * psx is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with amun. If not, see <http://www.gnu.org/licenses/>.
+ * along with psx. If not, see <http://www.gnu.org/licenses/>.
  */
 
 try
--- configuration.php
+++ configuration.php
@@ -1,13 +1,11 @@
 <?php
 /*
- *  $Id: configuration.php 636 2012-09-01 10:32:42Z k42b3.x@googlemail.com $
- *
  * psx
  * A object oriented and modular based PHP framework for developing
  * dynamic web applications. For the current version and informations
  * visit <http://phpsx.org>
  *
- * Copyright (c) 2010-2012 Christoph Kappestein <k42b3.x@gmail.com>
+ * Copyright (c) 2010-2013 Christoph Kappestein <k42b3.x@gmail.com>
  *
  * This file is part of psx. psx is free software: you can
  * redistribute it and/or modify it under the terms of the
