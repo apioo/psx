290a36f33528a00fce97e0c717282738ae5a6250
Christoph Kappestein <k42b3.x@gmail.com>
2013-06-04 23:10:52 +0200

    remove odata method

--- module/sample.php
+++ module/sample.php
@@ -7,22 +7,8 @@ class sample extends ViewAbstract
 {
 	public function onLoad()
 	{
-		$this->template->assign('title', 'PSX Framework');
-
-		$this->template->assign('subTitle', 'Template sample ...');
-
-		$this->template->set('sample.tpl');
-	}
-
-	/**
-	 * @httpMethod GET
-	 * @path /odata
-	 */
-	public function odata()
-	{
-		$person = new PSX\Payment\Paypal\Data\Payment();
-		$writer = new MetaDataWriter('foo');
-		$writer->addEntity($person);
-		$writer->close();
+		$this->getTemplate()->assign('title', 'PSX Framework');
+		$this->getTemplate()->assign('subTitle', 'Template sample ...');
+		$this->getTemplate()->set('sample.tpl');
 	}
 }
