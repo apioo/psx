b9cd34677ae1547ece10581f1ade6666051bf9c5
Christoph Kappestein <k42b3.x@gmail.com>
2013-10-26 22:41:46 +0200

    use real request method in oauth authentication filter

--- library/PSX/Dispatch/RequestFilter/OauthAuthentication.php
+++ library/PSX/Dispatch/RequestFilter/OauthAuthentication.php
@@ -122,7 +122,7 @@ class OauthAuthentication implements RequestFilterInterface
 				{
 					$signature = Oauth::getSignature($params['oauth_signature_method']);
 
-					$method = $request->getMethod();
+					$method = $_SERVER['REQUEST_METHOD'];
 					$url    = $request->getUrl();
 					$params = array_merge($params, $_GET);
 
