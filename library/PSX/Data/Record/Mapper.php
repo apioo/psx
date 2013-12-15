7dea77c3c2e35971d385ae37cbc80fe0905d6eca
Christoph Kappestein <k42b3.x@gmail.com>
2013-12-15 13:36:36 +0100

    add data mapper

---
+++ library/PSX/Data/Record/Mapper.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * psx
+ * A object oriented and modular based PHP framework for developing
+ * dynamic web applications. For the current version and informations
+ * visit <http://phpsx.org>
+ *
+ * Copyright (c) 2010-2013 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of psx. psx is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * psx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with psx. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PSX\Data\Record;
+
+use InvalidArgumentException;
+use PSX\Data\RecordInterface;
+
+/**
+ * Class wich can map all fields of an record to an arbitary class by calling
+ * the fitting setter methods if available
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link    http://phpsx.org
+ */
+class Mapper
+{
+	public function map(RecordInterface $source, $destination)
+	{
+		if(!is_object($destination))
+		{
+			throw new InvalidArgumentException('Destination must be an object');
+		}
+
+		$data = $source->getRecordInfo()->getData();
+
+		foreach($data as $key => $value)
+		{
+			// convert to camelcase if underscore is in name
+			if(strpos($key, '_') !== false)
+			{
+				$key = implode('', array_map('ucfirst', explode('_', $key)));
+			}
+
+			$method = 'set' . ucfirst($key);
+
+			if(is_callable(array($destination, $method)))
+			{
+				$destination->$method($value);
+			}
+		}
+	}
+}
+
---
+++ tests/PSX/Data/Record/MapperTest.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ * psx
+ * A object oriented and modular based PHP framework for developing
+ * dynamic web applications. For the current version and informations
+ * visit <http://phpsx.org>
+ *
+ * Copyright (c) 2010-2013 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of psx. psx is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * psx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with psx. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PSX\Data\Record;
+
+use DateTime;
+use PSX\Data\WriterTestRecord;
+
+/**
+ * MapperTest
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link    http://phpsx.org
+ */
+class MapperTest extends \PHPUnit_Framework_TestCase
+{
+	protected function setUp()
+	{
+	}
+
+	protected function tearDown()
+	{
+	}
+
+	public function testMap()
+	{
+		$source = new WriterTestRecord();
+		$source->setId(1);
+		// should not be mapped since the destination has no author setter
+		$source->setAuthor('bar');
+		$source->setTitle('foo');
+		$source->setDate(new DateTime());
+
+		$destination = new MapperDestinationRecord();
+
+		$mapper = new Mapper();
+		$mapper->map($source, $destination);
+
+		$this->assertEquals($source->getId(), $destination->getId());
+		$this->assertEquals($source->getTitle(), $destination->getTitle());
+		$this->assertEquals($source->getDate(), $destination->getDate());
+	}
+}
+
+class MapperDestinationRecord
+{
+	protected $id;
+	protected $title;
+	protected $date;
+
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+	
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function setTitle($title)
+	{
+		$this->title = $title;
+	}
+	
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	public function setDate($date)
+	{
+		$this->date = $date;
+	}
+	
+	public function getDate()
+	{
+		return $this->date;
+	}
+}
