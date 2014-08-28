f226088df81082a8c41e7fc3659d94190554b652
Christoph Kappestein <k42b3.x@gmail.com>
2014-08-28 23:50:47 +0200

    add propsed psr interfaces

---
+++ library/Psr/Cache/CacheException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Psr\Cache;
+
+/**
+ * Exception interface for all exceptions thrown by an Implementing Library.
+ */
+interface CacheException {}
---
+++ library/Psr/Http/Message/ResponseInterface.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Psr\Http\Message;
+
+/**
+ * A HTTP response message.
+ * @link http://tools.ietf.org/html/rfc2616#section-6
+ */
+interface ResponseInterface extends MessageInterface
+{
+    /**
+     * Gets the response Status-Code.
+     *
+     * The Status-Code is a 3-digit integer result code of the server's attempt
+     * to understand and satisfy the request.
+     *
+     * @return integer Status code.
+     */
+    public function getStatusCode();
+
+    /**
+     * Sets the status code of this response.
+     *
+     * @param integer $code The 3-digit integer result code to set.
+     */
+    public function setStatusCode($code);
+
+    /**
+     * Gets the response Reason-Phrase, a short textual description of the Status-Code.
+     *
+     * Because a Reason-Phrase is not a required element in response
+     * Status-Line, the Reason-Phrase value MAY be null. Implementations MAY
+     * choose to return the default RFC 2616 recommended reason phrase for the
+     * response's Status-Code.
+     *
+     * @return string|null Reason phrase, or null if unknown.
+     */
+    public function getReasonPhrase();
+
+    /**
+     * Sets the Reason-Phrase of the response.
+     *
+     * If no Reason-Phrase is specified, implementations MAY choose to default
+     * to the RFC 2616 recommended reason phrase for the response's Status-Code.
+     *
+     * @param string $phrase The Reason-Phrase to set.
+     */
+    public function setReasonPhrase($phrase);
+}
