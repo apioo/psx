eda768a205840751c49284f2902f38a3bbef3fef
Christoph Kappestein <k42b3.x@gmail.com>
2014-08-31 11:28:52 +0200

    renamed api documentation class

--- library/PSX/Data/Schema/Documentation.php
+++ library/PSX/Data/Schema/Documentation.php
@@ -26,43 +26,82 @@ namespace PSX\Data\Schema;
 use PSX\Data\SchemaInterface;
 
 /**
- * ApiDocumentation
+ * Documentation
  *
  * @author  Christoph Kappestein <k42b3.x@gmail.com>
  * @license http://www.gnu.org/licenses/gpl.html GPLv3
  * @link    http://phpsx.org
  */
-class ApiDocumentation
+class Documentation
 {
 	const METHOD_GET    = 0x0;
 	const METHOD_POST   = 0x1;
 	const METHOD_PUT    = 0x2;
 	const METHOD_DELETE = 0x3;
 
+	const TYPE_REQUEST  = 0x0;
+	const TYPE_RESPONSE = 0x1;
+
 	protected $container = array();
 
 	public function setGet(SchemaInterface $responseSchema)
 	{
-		$this->container[self::METHOD_GET] = [$responseSchema];
+		$this->container[self::METHOD_GET] = array(
+			self::TYPE_RESPONSE => $responseSchema
+		);
 	}
 
 	public function setPost(SchemaInterface $requestSchema, SchemaInterface $responseSchema = null)
 	{
-		$this->container[self::METHOD_POST] = [$requestSchema, $responseSchema];
+		$this->container[self::METHOD_POST] = array(
+			self::TYPE_REQUEST  => $requestSchema, 
+			self::TYPE_RESPONSE => $responseSchema
+		);
 	}
 
 	public function setPut(SchemaInterface $requestSchema, SchemaInterface $responseSchema = null)
 	{
-		$this->container[self::METHOD_PUT] = [$requestSchema, $responseSchema];
+		$this->container[self::METHOD_PUT] = array(
+			self::TYPE_REQUEST  => $requestSchema, 
+			self::TYPE_RESPONSE => $responseSchema
+		);
 	}
 
 	public function setDelete(SchemaInterface $requestSchema, SchemaInterface $responseSchema = null)
 	{
-		$this->container[self::METHOD_DELETE] = [$requestSchema, $responseSchema];
+		$this->container[self::METHOD_DELETE] = array(
+			self::TYPE_REQUEST  => $requestSchema, 
+			self::TYPE_RESPONSE => $responseSchema
+		);
+	}
+
+	public function getRequest($method)
+	{
+		return isset($this->container[$method][self::TYPE_REQUEST]) ? $this->container[$method][self::TYPE_REQUEST] : null;
+	}
+
+	public function hasRequest($method)
+	{
+		return isset($this->container[$method][self::TYPE_REQUEST]);
+	}
+
+	public function getResponse($method)
+	{
+		return isset($this->container[$method][self::TYPE_RESPONSE]) ? $this->container[$method][self::TYPE_RESPONSE] : null;
+	}
+
+	public function hasResponse($method)
+	{
+		return isset($this->container[$method][self::TYPE_RESPONSE]);
 	}
 
 	public function get($method)
 	{
 		return isset($this->container[$method]) ? $this->container[$method] : null;
+	}
+
+	public function has($method)
+	{
+		return isset($this->container[$method]);
 	}
 }
