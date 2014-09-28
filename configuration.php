1cfaadc81972c60079081bab5f0a0394be63a746
Christoph Kappestein <k42b3.x@gmail.com>
2014-09-28 19:31:34 +0200

    add comments to configuration

--- configuration.php
+++ configuration.php
@@ -23,18 +23,39 @@
 
 return array(
 
+	// The url to the psx public folder (i.e. http://127.0.0.1/psx/public)
 	'psx_url'                 => 'http://127.0.0.1/projects/psx/public',
+
+	// The input path 'index.php/' or '' if you use mod_rewrite
 	'psx_dispatch'            => 'index.php/',
+
+	// The default timezone
 	'psx_timezone'            => 'UTC',
+
+	// Whether PSX runs in debug mode or not. If not the error reporting is 
+	// set to 0
 	'psx_debug'               => true,
 
+	// Your SQL connections
 	'psx_sql_host'            => 'localhost',
 	'psx_sql_user'            => 'root',
 	'psx_sql_pw'              => '',
 	'psx_sql_db'              => 'psx',
 
+	// Path to the routing file
 	'psx_routing'             => __DIR__ . '/routes',
+
+	// Path to the cache folder
 	'psx_path_cache'          => __DIR__ . '/cache',
+
+	// Path to the library folder
 	'psx_path_library'        => __DIR__ . '/library',
+
+	// Class name of the error controller
+	//'psx_error_controller'    => null,
+
+	// If you only want to change the appearance of the error page you can 
+	// specify a custom template
+	//'psx_error_template'      => null,
 
 );
