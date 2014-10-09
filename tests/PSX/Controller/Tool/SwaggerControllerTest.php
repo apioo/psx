b1860f482407c9bebaa6fdd361994673fdc0949b
Christoph Kappestein <k42b3.x@gmail.com>
2014-10-09 19:10:47 +0200

    add tool controller test

---
+++ tests/PSX/Controller/Tool/SwaggerControllerTest.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * psx
+ * A object oriented and modular based PHP framework for developing
+ * dynamic web applications. For the current version and informations
+ * visit <http://phpsx.org>
+ *
+ * Copyright (c) 2010-2014 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of psx. psx is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * psx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with psx. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PSX\Controller\Tool;
+
+use PSX\Http\Stream\TempStream;
+use PSX\Http\Request;
+use PSX\Http\Response;
+use PSX\Json;
+use PSX\Test\ControllerTestCase;
+use PSX\Url;
+
+/**
+ * SwaggerControllerTest
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link    http://phpsx.org
+ */
+class SwaggerControllerTest extends ControllerTestCase
+{
+	public function testIndex()
+	{
+		$body     = new TempStream(fopen('php://memory', 'r+'));
+		$request  = new Request(new Url('http://127.0.0.1/swagger'), 'GET');
+		$request->addHeader('Accept', 'application/json');
+		$response = new Response();
+		$response->setBody($body);
+
+		$controller = $this->loadController($request, $response);
+		$data       = Json::decode((string) $body);
+
+		$this->assertArrayHasKey('swaggerVersion', $data);
+		$this->assertArrayHasKey('apiVersion', $data);
+		$this->assertArrayHasKey('apis', $data);
+		$this->assertTrue(is_array($data['apis']));
+
+		foreach($data['apis'] as $api)
+		{
+			$this->assertTrue(isset($api['path']));
+			$this->assertEquals('/1/api', $api['path']);
+		}
+	}
+
+	public function testDetail()
+	{
+		$body     = new TempStream(fopen('php://memory', 'r+'));
+		$request  = new Request(new Url('http://127.0.0.1/swagger/1/api'), 'GET');
+		$request->addHeader('Accept', 'application/json');
+		$response = new Response();
+		$response->setBody($body);
+
+		$controller = $this->loadController($request, $response);
+		$data       = Json::decode((string) $body);
+
+		$this->assertArrayHasKey('swaggerVersion', $data);
+		$this->assertArrayHasKey('apiVersion', $data);
+		$this->assertArrayHasKey('resourcePath', $data);
+		$this->assertArrayHasKey('apis', $data);
+		$this->assertArrayHasKey('models', $data);
+		$this->assertEquals('/api', $data['resourcePath']);
+	}
+
+	protected function getPaths()
+	{
+		return array(
+			[['GET'], '/swagger', 'PSX\Controller\Tool\SwaggerGeneratorController'],
+			[['GET'], '/swagger/:version/*path', 'PSX\Controller\Tool\SwaggerGeneratorController'],
+			[['GET', 'POST', 'PUT', 'DELETE'], '/api', 'PSX\Controller\Foo\Application\TestSchemaApiController'],
+		);
+	}
+}
