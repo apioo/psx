d5d510f86f51566698fb5297d1e19191fa355292
Christoph Kappestein <k42b3.x@gmail.com>
2014-10-30 22:43:37 +0100

    add default controller

---
+++ library/PSX/Controller/Resource/default_controller.tpl
@@ -0,0 +1,6 @@
+<!DOCTYPE>
+<html>
+<body>
+	<p><?php echo $message; ?> (<a href="<?php echo $url; ?>"><?php echo $url; ?></a>)</p>
+</body>
+</html>
---
+++ tests/PSX/Controller/Tool/DefaultControllerTest.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * psx
+ * A object oriented and modular based PHP framework for developing
+ * dynamic web applications. For the current version and informations
+ * visit <http://phpsx.org>
+ *
+ * Copyright (c) 2010-2014 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of psx. psx is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * psx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with psx. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PSX\Controller\Tool;
+
+use PSX\Http\Stream\TempStream;
+use PSX\Http\Request;
+use PSX\Http\Response;
+use PSX\Json;
+use PSX\Test\ControllerTestCase;
+use PSX\Url;
+
+/**
+ * DefaultControllerTest
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link    http://phpsx.org
+ */
+class DefaultControllerTest extends ControllerTestCase
+{
+	public function testIndex()
+	{
+		$body     = new TempStream(fopen('php://memory', 'r+'));
+		$request  = new Request(new Url('http://127.0.0.1/'), 'GET');
+		$request->addHeader('Accept', 'application/json');
+		$response = new Response();
+		$response->setBody($body);
+
+		$controller = $this->loadController($request, $response);
+		$data       = Json::decode((string) $body);
+
+		$this->assertArrayHasKey('message', $data);
+		$this->assertArrayHasKey('url', $data);
+
+		$this->assertEquals('This is the default controller of PSX', $data['message']);
+		$this->assertEquals('http://phpsx.org', $data['url']);
+	}
+
+	protected function getPaths()
+	{
+		return array(
+			[['GET'], '/', 'PSX\Controller\Tool\DefaultController'],
+		);
+	}
+}
