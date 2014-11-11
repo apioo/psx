94ff1dc3608854d633bb38bb954ad3dac0d00e2f
Christoph Kappestein <k42b3.x@gmail.com>
2014-11-11 22:34:23 +0100

    add serializer test

---
+++ tests/PSX/Data/Serializer/TestObjectVersioned.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * psx
+ * A object oriented and modular based PHP framework for developing
+ * dynamic web applications. For the current version and informations
+ * visit <http://phpsx.org>
+ *
+ * Copyright (c) 2010-2014 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of psx. psx is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * psx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with psx. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PSX\Data\Serializer;
+
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * TestObjectVersioned
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link    http://phpsx.org
+ *
+ * @JMS\ExclusionPolicy("none")
+ */
+class TestObjectVersioned
+{
+	/**
+	 * @JMS\Since("1")
+	 */
+	private $name;
+
+	/**
+	 * @JMS\Since("2")
+	 */
+	private $author;
+
+	public function setName($name)
+	{
+		$this->name = $name;
+	}
+	
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function setAuthor($author)
+	{
+		$this->author = $author;
+	}
+	
+	public function getAuthor()
+	{
+		return $this->author;
+	}
+}
---
+++ tests/PSX/Data/SerializerTest.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * psx
+ * A object oriented and modular based PHP framework for developing
+ * dynamic web applications. For the current version and informations
+ * visit <http://phpsx.org>
+ *
+ * Copyright (c) 2010-2014 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of psx. psx is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * psx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with psx. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PSX\Data;
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use PSX\Api\Version;
+
+/**
+ * SerializerTest
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link    http://phpsx.org
+ */
+class SerializerTest extends \PHPUnit_Framework_TestCase
+{
+	public function testSerialize()
+	{
+		$object = new Serializer\TestObject();
+		$object->setName('foo');
+		$object->setAuthor('bar');
+
+		$return = getContainer()->get('serializer')->serialize($object);
+
+		$this->assertEquals(array('name' => 'foo', 'author' => 'bar'), $return);
+	}
+
+	public function testSerializeVersioned()
+	{
+		$object = new Serializer\TestObjectVersioned();
+		$object->setName('foo');
+		$object->setAuthor('bar');
+
+		$return = getContainer()->get('serializer')->serialize($object, new Version(1));
+
+		$this->assertEquals(array('name' => 'foo'), $return);
+	}
+
+	public function testSerializeVersionedGreater()
+	{
+		$object = new Serializer\TestObjectVersioned();
+		$object->setName('foo');
+		$object->setAuthor('bar');
+
+		$return = getContainer()->get('serializer')->serialize($object, new Version(2));
+
+		$this->assertEquals(array('name' => 'foo', 'author' => 'bar'), $return);
+	}
+}
