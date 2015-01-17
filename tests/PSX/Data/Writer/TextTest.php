0629d491cdcf5e3d5c4e36f7673a0a5a7baa4e8a
Christoph Kappestein <k42b3.x@gmail.com>
2015-01-17 14:06:17 +0100

    add text writer

---
+++ tests/PSX/Data/Writer/TextTest.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * psx
+ * A object oriented and modular based PHP framework for developing
+ * dynamic web applications. For the current version and informations
+ * visit <http://phpsx.org>
+ *
+ * Copyright (c) 2010-2015 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of psx. psx is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * psx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with psx. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PSX\Data\Writer;
+
+use PSX\Http\MediaType;
+use PSX\Loader\ReverseRouter;
+use PSX\TemplateInterface;
+
+/**
+ * TextTest
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link    http://phpsx.org
+ */
+class TextTest extends TemplateAbstractTestCase
+{
+	protected function getWriter(TemplateInterface $template, ReverseRouter $router)
+	{
+		return new Text($template, $router);
+	}
+
+	public function testIsContentTypeSupported()
+	{
+		$template = $this->getMock('PSX\TemplateInterface');
+		$router   = $this->getMockBuilder('PSX\Loader\ReverseRouter')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$writer = new Text($template, $router);
+
+		$this->assertTrue($writer->isContentTypeSupported(MediaType::parse('text/plain')));
+		$this->assertFalse($writer->isContentTypeSupported(MediaType::parse('text/html')));
+	}
+
+	public function testGetContentType()
+	{
+		$template = $this->getMock('PSX\TemplateInterface');
+		$router   = $this->getMockBuilder('PSX\Loader\ReverseRouter')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$writer = new Text($template, $router);
+
+		$this->assertEquals('text/plain', $writer->getContentType());
+	}
+}
