beca400cc2ef6d305797f2b93d55ee52f3080b32
Christoph Kappestein <k42b3.x@gmail.com>
2015-02-09 22:13:49 +0100

    update doc

--- container.php
+++ container.php
@@ -2,9 +2,9 @@
 
 /*
 This file returns the global DI container for the application. The DI container
-which gets returned must be compatible with the symfony DI container interface.
-If you want load an different configuration depending on the environment you can 
-change the "config.file" parameter.
+must be compatible with the symfony DI container interface. If you want load an 
+different configuration depending on the environment you can change the 
+"config.file" parameter.
 */
 
 $container = new PSX\Dependency\DefaultContainer();
