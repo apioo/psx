c8a6abbe02701e91a7484c7048d050d602186999
Christoph Kappestein <k42b3.x@gmail.com>
2015-04-30 18:40:55 +0200

    add schema builder test case

---
+++ tests/PSX/Data/Schema/BuilderTest.php
@@ -0,0 +1,164 @@
+<?php
+/*
+ * PSX is a open source PHP framework to develop RESTful APIs.
+ * For the current version and informations visit <http://phpsx.org>
+ *
+ * Copyright 2010-2015 Christoph Kappestein <k42b3.x@gmail.com>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PSX\Data\Schema;
+
+/**
+ * BuilderTest
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+class BuilderTest extends \PHPUnit_Framework_TestCase
+{
+	public function testMeta()
+	{
+		$builder = new Builder('foo');
+
+		$builder
+			->setDescription('bar')
+			->setRequired(true)
+			->setReference('stdClass');
+
+		$property = $builder->getProperty();
+
+		$this->assertEquals('foo', $property->getName());
+		$this->assertEquals('bar', $property->getDescription());
+		$this->assertEquals(true, $property->isRequired());
+		$this->assertEquals('stdClass', $property->getReference());
+	}
+
+	public function testArrayType()
+	{
+		$builder = new Builder('foo');
+
+		$builder->arrayType('foo');
+		$builder->arrayType(new Property\ArrayType('bar'));
+
+		$property = $builder->getProperty();
+
+		$this->assertInstanceOf('PSX\Data\Schema\Property\ArrayType', $property->get('foo'));
+		$this->assertInstanceOf('PSX\Data\Schema\Property\ArrayType', $property->get('bar'));
+	}
+
+	public function testBoolean()
+	{
+		$builder = new Builder('foo');
+
+		$builder->boolean('foo');
+
+		$property = $builder->getProperty();
+
+		$this->assertInstanceOf('PSX\Data\Schema\Property\Boolean', $property->get('foo'));
+	}
+
+	public function testComplexType()
+	{
+		$builder = new Builder('foo');
+
+		$builder->complexType('foo');
+		$builder->complexType(new Property\ComplexType('bar'));
+		$builder->complexType('baz', new Property\ComplexType('foo'));
+
+		$property = $builder->getProperty();
+
+		$this->assertInstanceOf('PSX\Data\Schema\Property\ComplexType', $property->get('foo'));
+		$this->assertInstanceOf('PSX\Data\Schema\Property\ComplexType', $property->get('bar'));
+		$this->assertInstanceOf('PSX\Data\Schema\Property\ComplexType', $property->get('baz'));
+	}
+
+	public function testDate()
+	{
+		$builder = new Builder('foo');
+
+		$builder->date('foo');
+
+		$property = $builder->getProperty();
+
+		$this->assertInstanceOf('PSX\Data\Schema\Property\Date', $property->get('foo'));
+	}
+
+	public function testDateTime()
+	{
+		$builder = new Builder('foo');
+
+		$builder->dateTime('foo');
+
+		$property = $builder->getProperty();
+
+		$this->assertInstanceOf('PSX\Data\Schema\Property\DateTime', $property->get('foo'));
+	}
+
+	public function testDuration()
+	{
+		$builder = new Builder('foo');
+
+		$builder->duration('foo');
+
+		$property = $builder->getProperty();
+
+		$this->assertInstanceOf('PSX\Data\Schema\Property\Duration', $property->get('foo'));
+	}
+
+	public function testFloat()
+	{
+		$builder = new Builder('foo');
+
+		$builder->float('foo');
+
+		$property = $builder->getProperty();
+
+		$this->assertInstanceOf('PSX\Data\Schema\Property\Float', $property->get('foo'));
+	}
+
+	public function testInteger()
+	{
+		$builder = new Builder('foo');
+
+		$builder->integer('foo');
+
+		$property = $builder->getProperty();
+
+		$this->assertInstanceOf('PSX\Data\Schema\Property\Integer', $property->get('foo'));
+	}
+
+	public function testString()
+	{
+		$builder = new Builder('foo');
+
+		$builder->string('foo');
+
+		$property = $builder->getProperty();
+
+		$this->assertInstanceOf('PSX\Data\Schema\Property\String', $property->get('foo'));
+	}
+
+	public function testTime()
+	{
+		$builder = new Builder('foo');
+
+		$builder->time('foo');
+
+		$property = $builder->getProperty();
+
+		$this->assertInstanceOf('PSX\Data\Schema\Property\Time', $property->get('foo'));
+	}
+}
