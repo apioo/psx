4eca887684d358482464209411dd48db18865b0b
Christoph Kappestein <k42b3.x@gmail.com>
2016-09-10 19:09:31 +0200

    adjust config

--- configuration.php
+++ configuration.php
@@ -27,11 +27,15 @@ return array(
     'psx_log_handler'         => 'system',
     'psx_log_uri'             => null,
 
-    // Your SQL connections
-    'psx_sql_host'            => 'localhost',
-    'psx_sql_user'            => 'root',
-    'psx_sql_pw'              => '',
-    'psx_sql_db'              => 'psx',
+    // Database parameters which are used for the doctrine DBAL connection
+    // http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html
+    'psx_connection'          => [
+        'dbname'              => 'psx',
+        'user'                => 'root',
+        'password'            => '',
+        'host'                => 'localhost',
+        'driver'              => 'pdo_mysql',
+    ],
 
     // Path to the routing file
     'psx_routing'             => __DIR__ . '/routes',
