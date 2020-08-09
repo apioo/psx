e643aa3d84696b08bd543b132d811fa9fcc83be1
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-08-09 19:58:47 +0200

    use typeschema and remove raml

--- routes.php
+++ routes.php
@@ -5,10 +5,8 @@ return [
     # API
     [['ANY'], '/population/popo', \PSX\Framework\App\Api\Population\CollectionPopo::class],
     [['ANY'], '/population/popo/:id', \PSX\Framework\App\Api\Population\EntityPopo::class],
-    [['ANY'], '/population/jsonschema', \PSX\Framework\App\Api\Population\CollectionJsonSchema::class],
-    [['ANY'], '/population/jsonschema/:id', \PSX\Framework\App\Api\Population\EntityJsonSchema::class],
-    [['ANY'], '/population/raml', \PSX\Framework\App\Api\Population\CollectionRaml::class],
-    [['ANY'], '/population/raml/:id', \PSX\Framework\App\Api\Population\EntityRaml::class],
+    [['ANY'], '/population/typeschema', \PSX\Framework\App\Api\Population\CollectionTypeSchema::class],
+    [['ANY'], '/population/typeschema/:id', \PSX\Framework\App\Api\Population\EntityTypeSchema::class],
     [['ANY'], '/population/openapi', \PSX\Framework\App\Api\Population\CollectionOpenAPI::class],
     [['ANY'], '/population/openapi/:id', \PSX\Framework\App\Api\Population\EntityOpenAPI::class],
     [['ANY'], '/population', \PSX\Framework\App\Api\Population\Collection::class],
