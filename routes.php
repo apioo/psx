486ef6a2d7b00465c540ba340d514ab4fd2ef5ab
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-08-09 20:03:12 +0200

    add type schema generator

--- routes.php
+++ routes.php
@@ -19,8 +19,8 @@ return [
     [['GET'], '/tool/doc/:version/*path', \PSX\Framework\Controller\Tool\Documentation\DetailController::class],
     [['GET'], '/tool/routing', \PSX\Framework\Controller\Tool\RoutingController::class],
 
-    [['GET'], '/generator/raml/:version/*path', \PSX\Framework\Controller\Generator\RamlController::class],
-    [['GET'], '/generator/swagger/:version/*path', \PSX\Framework\Controller\Generator\SwaggerController::class],
     [['GET'], '/generator/openapi/:version/*path', \PSX\Framework\Controller\Generator\OpenAPIController::class],
+    [['GET'], '/generator/raml/:version/*path', \PSX\Framework\Controller\Generator\RamlController::class],
+    [['GET'], '/generator/typeschema/:version/*path', \PSX\Framework\Controller\Generator\TypeSchemaController::class],
 
 ];
