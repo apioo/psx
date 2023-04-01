4fbee19d579bc76f13288e511e53933962a4ef3b
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-04-01 13:05:36 +0200

    use env

--- configuration.php
+++ configuration.php
@@ -1,35 +1,40 @@
 <?php
 
-/*
-This is the configuration file of PSX. Every parameter can be used inside your
-application or in the DI container. Which configuration file gets loaded depends 
-on the DI container parameter "config.file". See the container.php if you want 
-load a different configuration depending on the environment.
-*/
+use Monolog\Logger;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
-return array(
+return [
 
-    // The url to the psx public folder (i.e. http://127.0.0.1/psx/public, 
-    // http://localhost.com or //localhost.com)
-    'psx_url'                 => 'http://127.0.0.1/projects/psx/public',
+    // The url to the psx public folder (i.e. http://api.acme.com or http://127.0.0.1/psx/public)
+    'psx_url'                 => env('APP_URL')->string(),
 
-    // The default timezone
-    'psx_timezone'            => 'UTC',
+    // The input path 'index.php/' or '' if every request is served to the index.php file
+    'psx_dispatch'            => '',
 
-    // Whether PSX runs in debug mode or not. If not error reporting is set to 0
-    // Also several caches are used if the debug mode is false
-    'psx_debug'               => true,
+    // Defines the current environment i.e. prod or dev
+    'psx_env'                 => env('APP_ENV')->string()->default('prod'),
+
+    // Whether the app runs in debug mode or not. If not error reporting is set to 0, also several caches are used if
+    // the debug mode is false
+    'psx_debug'               => env('APP_DEBUG')->bool()->default(false),
 
     // Database parameters which are used for the doctrine DBAL connection
-    // http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html
-    'psx_connection'          => getConnectionParams(getenv('DB')),
+    // https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html
+    'psx_connection'          => [
+        'dbname'              => env('APP_DB_NAME')->string(),
+        'user'                => env('APP_DB_USER')->string(),
+        'password'            => env('APP_DB_PW')->string(),
+        'host'                => env('APP_DB_HOST')->string()->default('localhost'),
+        'driver'              => env('APP_DB_DRIVER')->string()->default('pdo_mysql'),
+    ],
 
-    // Path to the routing file
-    'psx_routing'             => __DIR__ . '/routes.php',
+    // The log level
+    'psx_log_level'           => Logger::ERROR,
 
     // Folder locations
     'psx_path_cache'          => __DIR__ . '/cache',
-    'psx_path_library'        => __DIR__ . '/src',
+    'psx_path_src'            => __DIR__ . '/src',
+    'psx_path_log'            => __DIR__ . '/log',
 
     // Supported writers
     'psx_supported_writer'    => [
@@ -38,57 +43,4 @@ return array(
         \PSX\Data\Writer\Jsonx::class,
     ],
 
-    // Global middleware which are applied before and after every request. Must
-    // bei either a classname, closure or PSX\Dispatch\FilterInterface instance
-    //'psx_filter_pre'          => [],
-    //'psx_filter_post'         => [],
-
-    // A closure which returns a doctrine cache implementation. If null the
-    // filesystem cache is used
-    //'psx_cache_factory'       => null,
-
-    // A closure which returns a monolog handler implementation. If null the
-    // system handler is used
-    //'psx_logger_factory'      => null,
-
-    // Class name of the error controller
-    //'psx_error_controller'    => null,
-
-    // If you only want to change the appearance of the error page you can 
-    // specify a custom template
-    //'psx_error_template'      => null,
-
-);
-
-function getConnectionParams($db)
-{
-    switch ($db) {
-        case 'mysql':
-            return [
-                'dbname'   => 'psx',
-                'user'     => 'root',
-                'password' => 'test1234',
-                'host'     => 'localhost',
-                'driver'   => 'pdo_mysql',
-            ];
-            break;
-
-        case 'pgsql':
-            return [
-                'dbname'   => 'psx',
-                'user'     => 'postgres',
-                'password' => 'test1234',
-                'host'     => 'localhost',
-                'driver'   => 'pdo_pgsql',
-            ];
-            break;
-
-        default:
-        case 'sqlite':
-            return [
-                'path'   => __DIR__ . '/cache/population.db',
-                'driver' => 'pdo_sqlite',
-            ];
-            break;
-    }
-}
+];
--- container.php
+++ container.php
@@ -2,7 +2,7 @@
 
 return \PSX\Framework\Dependency\ContainerBuilder::build(
     __DIR__,
-    getenv('PSX_ENV') !== 'prod',
+    true,
     __DIR__ . '/vendor/psx/framework/resources/container.php',
     __DIR__ . '/resources/container.php',
 );
