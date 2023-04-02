f08328ed59a3530e29d290fea088e85183ff8bb9
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-04-02 20:59:02 +0200

    update gen

--- src/Table/Generated/PopulationRow.php
+++ src/Table/Generated/PopulationRow.php
@@ -17,7 +17,7 @@ class PopulationRow implements \JsonSerializable, \PSX\Record\RecordableInterfac
     }
     public function getId() : int
     {
-        return $this->id;
+        return $this->id ?? throw new \PSX\Sql\Exception\NoValueAvailable('No value for required column "id" was provided');
     }
     public function setPlace(int $place) : void
     {
@@ -25,7 +25,7 @@ class PopulationRow implements \JsonSerializable, \PSX\Record\RecordableInterfac
     }
     public function getPlace() : int
     {
-        return $this->place;
+        return $this->place ?? throw new \PSX\Sql\Exception\NoValueAvailable('No value for required column "place" was provided');
     }
     public function setRegion(string $region) : void
     {
@@ -33,7 +33,7 @@ class PopulationRow implements \JsonSerializable, \PSX\Record\RecordableInterfac
     }
     public function getRegion() : string
     {
-        return $this->region;
+        return $this->region ?? throw new \PSX\Sql\Exception\NoValueAvailable('No value for required column "region" was provided');
     }
     public function setPopulation(int $population) : void
     {
@@ -41,7 +41,7 @@ class PopulationRow implements \JsonSerializable, \PSX\Record\RecordableInterfac
     }
     public function getPopulation() : int
     {
-        return $this->population;
+        return $this->population ?? throw new \PSX\Sql\Exception\NoValueAvailable('No value for required column "population" was provided');
     }
     public function setUsers(int $users) : void
     {
@@ -49,7 +49,7 @@ class PopulationRow implements \JsonSerializable, \PSX\Record\RecordableInterfac
     }
     public function getUsers() : int
     {
-        return $this->users;
+        return $this->users ?? throw new \PSX\Sql\Exception\NoValueAvailable('No value for required column "users" was provided');
     }
     public function setWorldUsers(float $worldUsers) : void
     {
@@ -57,7 +57,7 @@ class PopulationRow implements \JsonSerializable, \PSX\Record\RecordableInterfac
     }
     public function getWorldUsers() : float
     {
-        return $this->worldUsers;
+        return $this->worldUsers ?? throw new \PSX\Sql\Exception\NoValueAvailable('No value for required column "world_users" was provided');
     }
     public function setInsertDate(\PSX\DateTime\LocalDateTime $insertDate) : void
     {
@@ -65,10 +65,11 @@ class PopulationRow implements \JsonSerializable, \PSX\Record\RecordableInterfac
     }
     public function getInsertDate() : \PSX\DateTime\LocalDateTime
     {
-        return $this->insertDate;
+        return $this->insertDate ?? throw new \PSX\Sql\Exception\NoValueAvailable('No value for required column "insert_date" was provided');
     }
     public function toRecord() : \PSX\Record\RecordInterface
     {
+        /** @var \PSX\Record\Record<mixed> $record */
         $record = new \PSX\Record\Record();
         $record->put('id', $this->id);
         $record->put('place', $this->place);
@@ -86,13 +87,13 @@ class PopulationRow implements \JsonSerializable, \PSX\Record\RecordableInterfac
     public static function from(array|\ArrayAccess $data) : self
     {
         $row = new self();
-        $row->id = $data['id'] ?? null;
-        $row->place = $data['place'] ?? null;
-        $row->region = $data['region'] ?? null;
-        $row->population = $data['population'] ?? null;
-        $row->users = $data['users'] ?? null;
-        $row->worldUsers = $data['world_users'] ?? null;
-        $row->insertDate = isset($data['insert_date']) ? \PSX\DateTime\LocalDateTime::from($data['insert_date']) : null;
+        $row->id = isset($data['id']) && is_int($data['id']) ? $data['id'] : null;
+        $row->place = isset($data['place']) && is_int($data['place']) ? $data['place'] : null;
+        $row->region = isset($data['region']) && is_string($data['region']) ? $data['region'] : null;
+        $row->population = isset($data['population']) && is_int($data['population']) ? $data['population'] : null;
+        $row->users = isset($data['users']) && is_int($data['users']) ? $data['users'] : null;
+        $row->worldUsers = isset($data['world_users']) && is_float($data['world_users']) ? $data['world_users'] : null;
+        $row->insertDate = isset($data['insert_date']) && $data['insert_date'] instanceof \DateTimeInterface ? \PSX\DateTime\LocalDateTime::from($data['insert_date']) : null;
         return $row;
     }
 }
