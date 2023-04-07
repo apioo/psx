96563fa33742d5a7f5cfbac61dfd2640b8a762ec
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-04-07 16:53:40 +0200

    update docs

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
  * PSX is a open source PHP framework to develop RESTful APIs.
  * For the current version and informations visit <http://phpsx.org>
  *
- * Copyright 2010-2020 Christoph Kappestein <christoph.kappestein@gmail.com>
+ * Copyright 2010-2023 Christoph Kappestein <christoph.kappestein@gmail.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
