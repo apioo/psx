5bea81249c0a4ef78079f3363e6e33f9a7873465
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-02-25 00:25:20 +0100

    update bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $container = require_once __DIR__ . '/../container.php';
 
-// connection used for testing
-$params = [
-    'memory' => true,
-    'driver' => 'pdo_sqlite',
-];
-
 /** @var \PSX\Framework\Test\Environment $environment */
 $environment = $container->get(\PSX\Framework\Test\Environment::class);
-$environment->setup($params);
+$environment->setup();
